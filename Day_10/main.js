8e602378705407569c5d9aef0e9caa86bfd3b16a
ayaabbas286 <ayaabbas286@gmail.com>
2025-02-13 17:55:13 +0200

    @Day10

---
+++ Day_10/main.js
@@ -0,0 +1,30 @@
+var user = document.getElementById("name");
+function changeName() {
+  var username = prompt("Please enter your username");
+  user.textContent = username;
+}
+var photo = document.getElementById("photo");
+function on() {
+  photo.src = "/Day_10/pic_bulbon.gif";
+}
+function off() {
+  photo.src = "/Day_10/pic_bulboff.gif";
+}
+var Res = document.getElementById("result");
+function calculate() {
+  var scoreInput = document.getElementById("gpa").value;
+  if (scoreInput >= 90) {
+    Res.textContent = "Grad: A for / " + scoreInput;
+  } else if (scoreInput >= 80) {
+    Res.textContent = "Grad: B for / " + scoreInput;
+  } else if (scoreInput >= 70) {
+    Res.textContent = "Grad: C for / " + scoreInput;
+  } else scoreInput >= 50;
+  Res.textContent = "Grad: F for / " + scoreInput;
+}
+function product_cost(quatity, price) {
+  var price = document.getElementById("price").value;
+  var quatity = document.getElementById("quantity").value;
+  var cost = document.getElementById("cost");
+  cost.textContent = "" + price * quatity;
+}
